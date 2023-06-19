78b744e512c3b0d1d2c2f20725f8a26a0e8ebad5
Aleksey <petrov.legeon@yandex.ru>
2023-06-20 02:02:11 +0300

    Фикс ошибок

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { errors } = require('celebrate');
-const path = require('path');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
@@ -22,8 +21,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
 app.use(
   cors({
     origin: [
-      'http://localhost:3000',
       'http://localhost:3001',
+      'http://alpetrov.dip.nomoredomains.work',
+      'https://alpetrov.dip.nomoredomains.work',
+      'http://api.alpetrov.dip.nomoredomains.work',
+      'https://api.alpetrov.dip.nomoredomains.work',
     ],
     credentials: true,
     maxAge: 60,
@@ -34,7 +36,6 @@ app.use(helmet());
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(requestLogger);
